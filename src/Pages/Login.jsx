7c47e82c56b3015500ae5138c075dbac4400822e
hassan1032 <khanhassan1032@gmail.com>
2024-06-15 12:34:42 +0530

    change test

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,6 +10,7 @@ export function Login() {
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
+            /*jhh*/
             <p className="text-balance text-muted-foreground">
               Enter your email below to login to your account
             </p>
