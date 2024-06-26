f2888050d18c2777a2fd212ddefebd24ba57cedb
hassan1032 <khanhassan1032@gmail.com>
2024-06-26 13:00:09 +0530

    changes files

--- src/Pages/sub-component/Messages.jsx
+++ src/Pages/sub-component/Messages.jsx
@@ -1,8 +1,3 @@
-// Messages.js
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import {
   clearAllMessageErrors,
@@ -19,14 +15,29 @@ import {
   getAllMessages,
   resetMessagesSlice,
 } from "../../store/slices/message.Slice";
-import SpecialLoadingButton from"../sub-component/specialLoding.button";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import SpecialLoadingButton from "../sub-component/specialLoding.button";
+import { useNavigate } from "react-router-dom";
 
 const Messages = () => {
-  const dispatch = useDispatch();
   const navigateTo = useNavigate();
-  const { messages, loading, error, message } = useSelector((state) => state.messages);
-  const [messageId, setMessageId] = useState("");
+  const handleReturnToDashboard = () => {
+    navigateTo("/");
+  };
 
+  const { messages, loading, error, message } = useSelector(
+    (state) => state.messages
+  );
+
+  const [messageId, setMessageId] = useState("");
+  const handleMessageDelete = (id) => {
+    setMessageId(id);
+    dispatch(deleteMessage(id));
+  };
+
+  const dispatch = useDispatch();
   useEffect(() => {
     if (error) {
       toast.error(error);
@@ -37,63 +48,66 @@ const Messages = () => {
       dispatch(resetMessagesSlice());
       dispatch(getAllMessages());
     }
-  }, [dispatch, error, message]);
-
-  const handleReturnToDashboard = () => {
-    navigateTo("/");
-  };
-
-  const handleMessageDelete = (id) => {
-    setMessageId(id);
-    dispatch(deleteMessage(id));
-  };
+  }, [dispatch, error, message, loading]);
 
   return (
-    <div className="min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-20">
-      <Tabs>
-        <TabsContent>
-          <Card>
-            <CardHeader className="flex gap-4 sm:justify-between sm:flex-row sm:items-center">
-              <CardTitle>Messages</CardTitle>
-              <Button className="w-fit" onClick={handleReturnToDashboard}>
-                Return to Dashboard
-              </Button>
-            </CardHeader>
-            <CardContent className="grid sm:grid-cols-2 gap-4">
-              {messages && messages.length > 0 ? (
-                messages.map((element) => (
-                  <Card key={element._id} className="grid gap-2">
-                    <CardDescription className="text-slate-950">
-                      <span className="font-bold mr-2">Sender Name:</span>
-                      {element.senderName}
-                    </CardDescription>
-                    <CardDescription className="text-slate-950">
-                      <span className="font-bold mr-2">Subject:</span>
-                      {element.subject}
-                    </CardDescription>
-                    <CardDescription className="text-slate-950">
-                      <span className="font-bold mr-2">Message:</span>
-                      {element.message}
-                    </CardDescription>
-                    <CardFooter className="justify-end">
-                      {loading && messageId === element._id ? (
-                        <SpecialLoadingButton content={"Deleting"} width={"w-32"} />
-                      ) : (
-                        <Button className="w-32" onClick={() => handleMessageDelete(element._id)}>
-                          Delete
-                        </Button>
-                      )}
-                    </CardFooter>
-                  </Card>
-                ))
-              ) : (
-                <CardHeader className="text-2xl">No Messages Found!</CardHeader>
-              )}
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <div className="min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-20">
+        <Tabs>
+          <TabsContent>
+            <Card>
+              <CardHeader className="flex gap-4 sm:justify-between sm:flex-row sm:items-center">
+                <CardTitle>Messages</CardTitle>
+                <Button className="w-fit" onClick={handleReturnToDashboard}>
+                  Return to Dashboard
+                </Button>
+              </CardHeader>
+              <CardContent className="grid sm:grid-cols-2 gap-4">
+                {messages && messages.length > 0 ? (
+                  messages.map((element) => {
+                    return (
+                      <Card key={element._id} className="grid gap-2">
+                        <CardDescription className="text-slate-950">
+                          <span className="font-bold mr-2">Sender Name:</span>
+                          {element.senderName}
+                        </CardDescription>
+                        <CardDescription className="text-slate-950">
+                          <span className="font-bold mr-2">Subject:</span>
+                          {element.subject}
+                        </CardDescription>
+                        <CardDescription className="text-slate-950">
+                          <span className="font-bold mr-2">Message:</span>
+                          {element.message}
+                        </CardDescription>
+                        <CardFooter className="justify-end">
+                          {loading && messageId === element._id ? (
+                            <SpecialLoadingButton
+                              content={"Deleting"}
+                              width={"w-32"}
+                            />
+                          ) : (
+                            <Button
+                              className="w-32"
+                              onClick={() => handleMessageDelete(element._id)}
+                            >
+                              Delete
+                            </Button>
+                          )}
+                        </CardFooter>
+                      </Card>
+                    );
+                  })
+                ) : (
+                  <CardHeader className="text-2xl">
+                    No Messages Found!
+                  </CardHeader>
+                )}
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
