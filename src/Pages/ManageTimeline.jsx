3ec1d8e010a62248eb1d8e11865dfc838dd6380e
hassan1032 <khanhassan1032@gmail.com>
2024-07-02 11:23:05 +0530

    Add new code

--- src/Pages/ManageTimeline.jsx
+++ src/Pages/ManageTimeline.jsx
@@ -1,36 +1,40 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
-import SpecialLoadingButton from "../Pages/sub-component/specialLoding.button";;
-import { Textarea } from "@/components/ui/textarea";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  addNewTimeline,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { Trash2 } from "lucide-react";
+import {
   clearAllTimelineErrors,
+  deleteTimeline,
   getAllTimeline,
   resetTimelineSlice,
 } from "@/store/slices/timelineSlice";
 
-const AddTimeline = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
+const ManageTimeline = () => {
+  const navigateTo = useNavigate();
+  const handleReturnToDashboard = () => {
+    navigateTo("/");
+  };
+  const { loading, timeline, error, message } = useSelector(
+    (state) => state.timeline
+  );
+  const dispatch = useDispatch();
 
-  const { loading, error, message } = useSelector((state) => state.timeline);
-
-  const handleAddNewTimeline = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("description", description);
-    formData.append("from", from);
-    formData.append("to", to);
-    dispatch(addNewTimeline(formData));
+  const handleDeleteTimeline = (id) => {
+    dispatch(deleteTimeline(id));
   };
 
-  const dispatch = useDispatch();
   useEffect(() => {
     if (error) {
       toast.error(error);
@@ -41,105 +45,72 @@ const AddTimeline = () => {
       dispatch(resetTimelineSlice());
       dispatch(getAllTimeline());
     }
-  }, [dispatch, error, message, loading]);
+  }, [dispatch, loading, error]);
 
   return (
-    <>
-      <div className="flex justify-center items-center min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-14">
-        <form
-          className="w-[100%] px-5 md:w-[650px]"
-          onSubmit={handleAddNewTimeline}
-        >
-          <div className="space-y-12">
-            <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="font-semibold leading-7 text-gray-900 text-3xl text-center">
-                ADD A NEW TIMELINE
-              </h2>
-              <div className="mt-10 flex flex-col gap-5">
-                <div className="w-full sm:col-span-4">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Title
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                      <input
-                        type="text"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Matriculation"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:col-span-4">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Description
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                      <Textarea
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:col-span-4">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Starting Point (From)
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                      <Input
-                        type="number"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="From"
-                        value={from}
-                        onChange={(e) => setFrom(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full sm:col-span-4">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Ending Point (To)
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                      <input
-                        type="number"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="To"
-                        value={to}
-                        onChange={(e) => setTo(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            {!loading ? (
-              <Button
-                type="submit"
-                onClick={() => handleAddNewSkill()}
-                className="w-full"
-              >
-                Add Timeline
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <Tabs defaultValue="week">
+        <TabsContent value="week">
+          <Card>
+            <CardHeader className="flex gap-4 sm:justify-between sm:flex-row sm:items-center">
+              <CardTitle>Manage Your Timeline</CardTitle>
+              <Button className="w-fit" onClick={handleReturnToDashboard}>
+                Return to Dashboard
               </Button>
-            ) : (
-              <SpecialLoadingButton content={"Adding New Skill"} />
-            )}
-          </div>
-        </form>
-      </div>
-    </>
+            </CardHeader>
+            <CardContent className="grid grid-cols-1 gap-4">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Title</TableHead>
+                    <TableHead className="md:table-cell">Description</TableHead>
+                    <TableHead className="md:table-cell">From</TableHead>
+                    <TableHead className="md:table-cell">To</TableHead>
+                    <TableHead className="text-right">Action</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {timeline.length > 0 ? (
+                    timeline.map((element) => {
+                      return (
+                        <TableRow className="bg-accent" key={element._id}>
+                          <TableCell className="font-medium">
+                            {element.title}
+                          </TableCell>
+                          <TableCell className="md:table-cell">
+                            {element.description}
+                          </TableCell>
+                          <TableCell className="md:table-cell">
+                            {element.timeline.from}
+                          </TableCell>
+                          <TableCell className="md:table-cell">
+                            {element.timeline.to ? element.timeline.to : "____"}
+                          </TableCell>
+                          <TableCell className="flex justify-end">
+                            <button
+                              className="border-red-600 border-2 rounded-full h-8 w-8 flex 
+                              justify-center items-center text-red-600  hover:text-slate-50 hover:bg-red-600"
+                              onClick={() => handleDeleteTimeline(element._id)}
+                            >
+                              <Trash2 className="h-5 w-5" />
+                            </button>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })
+                  ) : (
+                    <TableRow className="text-2xl">
+                      <TableCell>You have not added any timeline.</TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
-export default AddTimeline;
+export default ManageTimeline;
